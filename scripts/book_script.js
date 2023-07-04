b88ddc4e45e260935b55885057e7b8397f8ea65d
A Q <aqaraniv@students.kennesaw.edu>
2023-07-04 01:49:04 -0400

    add milestone two

---
+++ scripts/book_script.js
@@ -0,0 +1,23 @@
+function displayBookInfo (id) {
+	 $(document).ready(function() {
+		 var url = 'https://www.googleapis.com/books/v1/volumes/' + id
+		  $.getJSON(url, function(json) {
+			  $('#single_book').html('<image width="100" height="100" src="' + json.volumeInfo.imageLinks.smallThumbnail + '" />');
+			  $('#single_book').append('<p> <strong>Title :</strong> ' + json.volumeInfo.title+ '</p>');
+			  $('#single_book').append('<p> <strong>Subtitle:</strong> ' + json.volumeInfo.subtitle+ '</p>');
+			  $('#single_book').append('<p> <strong>Author:</strong> ' + json.volumeInfo.authors[0]+ '</p>');
+			  $('#single_book').append('<p> <strong>Publisher:</strong> ' + json.volumeInfo.publisher+ '</p>');
+			  $('#single_book').append('<p> <strong>Published Date:</strong> ' + json.volumeInfo.publishedDate+ '</p>');
+			  $('#single_book').append('<p> <strong>Description:</strong> ' + json.volumeInfo.description+ '</p>');
+			  $('#single_book').append('<p> <strong>ISBN_10:</strong> ' + json.volumeInfo.industryIdentifiers[0].identifier+ '</p>');
+			  $('#single_book').append('<p> <strong>ISBN_13:</strong> ' + json.volumeInfo.industryIdentifiers[1].identifier+ '</p>');
+			  $('#single_book').append('<p> <strong>Pages:</strong> ' + json.volumeInfo.pageCount+ '</p>');
+			  $('#single_book').append('<p> <strong>Average Rating:</strong> ' + json.volumeInfo.averageRating+ '</p>');
+			  $('#single_book').append('<p> <strong>Number of Ratings:</strong> ' + json.volumeInfo.ratingsCount+ '</p>');
+			  $('#single_book').append('<p> <strong>Maturity:</strong> ' + json.volumeInfo.maturityRating+ '</p>');
+			  $('#single_book').append('<p> <strong>Language:</strong> ' + json.volumeInfo.language+ '</p>');
+			  $('#single_book').append('<p> <strong>Country:</strong> ' + json.saleInfo.country+ '</p>');
+			  $('#single_book').append('<p> <strong>Saleability:</strong> ' + json.saleInfo.saleability+ '</p>');
+		   });
+	 });
+}
